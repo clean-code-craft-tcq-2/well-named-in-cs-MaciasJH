8fe0f1feb9c99820d8b2a8dafc2bf5f785b8068e
Joel Antonio Macias Hernandez <macias.joelh@gmail.com>
2022-01-10 19:18:05 -0600

    Modularity Exercise - Finished. - Separated functions and classes on different files. - Added functionality to write the whole color manual.

---
+++ TelCo.ColorCoder/WritePair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static TelCo.ColorCoder.GetColor;
+
+namespace TelCo.ColorCoder
+{
+    class WritePair
+    {
+        public static void PrintPair(int pairNumber)
+        {
+            ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+            Console.WriteLine("Pair Number: {0}, Colors: {1}, {2}\n", pairNumber, colorPair.majorColor.Name, colorPair.minorColor.Name);
+        }
+    }
+}
